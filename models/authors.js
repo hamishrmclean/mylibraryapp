6f0ff1f3c42d9bd5c3f9bb3a2bfca9ff5c243e97
hamishmclean <mcleanh1@student.unimelb.edu.au>
2020-04-25 20:41:48 +1000

    connected everything

--- models/authors.js
+++ models/authors.js
@@ -1,10 +1,10 @@
-module.exports = [
-    {
-        "id":"10001",
-        "first_name":"Jennifer",
-        "last_name":"Robbins",
-    }, {
-        "id":"10002",
-        "first_name":"Evan",
-        "last_name":"Hahn",
-    } ];
+const mongoose = require("mongoose");
+
+const authorSchema = new mongoose.Schema({
+    id: String,
+    first_name: String,
+    last_name: String
+});
+
+const Author = mongoose.model("author", authorSchema, "authors");
+module.exports = Author;
